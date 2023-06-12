2ace1280458930cb456f606eaf24865428d05104
somi4219 <somi4219@gmail.com>
2023-06-13 01:26:55 +0900

    경로 출력 기능 업데이트

---
+++ app/src/main/java/org/techtown/user/AStarAlgorithm.java
@@ -0,0 +1,196 @@
+package org.techtown.user;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class AStarAlgorithm {
+
+    public class Node implements Comparable<Node> {
+        int row; // 노드의 행 위치
+        int col; // 노드의 열 위치
+        int fScore; // f 스코어 (g 스코어와 휴리스틱을 합한 값)
+        int gScore; // g 스코어 (시작 노드부터 현재 노드까지의 이동 비용)
+        Node parent; // 경로 추적을 위한 부모 노드
+        private int numCols; // 미로의 열 수
+        private int numRows; // 미로의 행 수
+
+        public Node(int row, int col) {
+            this.row = row;
+            this.col = col;
+            this.fScore = Integer.MAX_VALUE;
+            this.gScore = Integer.MAX_VALUE;
+            this.parent = null;
+        }
+
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.fScore, other.fScore);
+        }
+    }
+
+    private int[][] maze; // 미로 정보를 저장하는 2차원 배열
+    private int numRows; // 미로의 행 수
+    private int numCols; // 미로의 열 수
+    private Node[][] nodes; // 각 위치에 대한 노드 정보를 저장하는 2차원 배열
+    private int[][] heuristic; // 각 위치에 대한 휴리스틱 값을 저장하는 2차원 배열
+
+    public AStarAlgorithm(int[][] maze) {
+        this.maze = maze;
+        this.numRows = maze.length;
+        this.numCols = maze[0].length;
+        this.nodes = new Node[numRows][numCols];
+        this.heuristic = new int[numRows][numCols];
+
+        initializeNodesAndHeuristic();
+    }
+
+    private void initializeNodesAndHeuristic() {
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                nodes[i][j] = new Node(i, j);
+                heuristic[i][j] = Integer.MAX_VALUE;
+            }
+        }
+    }
+
+    public List<Node> findShortestPath(int startRow, int startCol, int endRow, int endCol) {
+        Node startNode = nodes[startRow][startCol];
+        Node endNode = nodes[endRow][endCol];
+
+        PriorityQueue<Node> openSet = new PriorityQueue<>();
+        List<Node> closedSet = new ArrayList<>();
+
+        startNode.gScore = 0;
+        startNode.fScore = calculateFScore(startNode, endNode);
+
+        openSet.add(startNode);
+
+        while (!openSet.isEmpty()) {
+            Node current = openSet.poll();
+
+            if (current == endNode) {
+                return reconstructPath(current);
+            }
+
+            closedSet.add(current);
+
+            List<Node> neighbors = getNeighbors(current, endNode);
+            for (Node neighbor : neighbors) {
+                if (closedSet.contains(neighbor)) {
+                    continue;
+                }
+
+                int tentativeGScore = current.gScore + 1;
+
+                if (tentativeGScore < neighbor.gScore) {
+                    neighbor.parent = current;
+                    neighbor.gScore = tentativeGScore;
+                    neighbor.fScore = tentativeGScore + calculateFScore(neighbor, endNode);
+
+                    if (!openSet.contains(neighbor)) {
+                        openSet.add(neighbor);
+                    } else {
+                        // openSet에 이미 있는 neighbor의 fScore가 갱신되었으므로 우선순위 큐를 업데이트합니다.
+                        openSet.remove(neighbor);
+                        openSet.add(neighbor);
+                    }
+                }
+            }
+        }
+
+        return null; // 도착 노드에 도달할 수 없는 경우 빈 리스트 반환
+    }
+
+
+    private List<Node> reconstructPath(Node endNode) {
+        List<Node> path = new ArrayList<>();
+        Node current = endNode;
+
+        while (current != null) {
+            path.add(current);
+            current = current.parent;
+        }
+
+        Collections.reverse(path);
+        return path;
+    }
+
+    private boolean isValidLocation(int row, int col) {
+        return row >= 0 && row < numRows && col >= 0 && col < numCols && maze[row][col] != 0;
+    }
+
+    private List<Node> getNeighbors(Node node, Node endNode) {
+        List<Node> neighbors = new ArrayList<>();
+
+        int[][] directions = {
+                {-1, 0}, {1, 0}, {0, -1}, {0, 1}, // 상하좌우 방향
+                {-1, -1}, {-1, 1}, {1, -1}, {1, 1} // 대각선 방향
+        };
+
+        for (int[] direction : directions) {
+            int row = node.row + direction[0];
+            int col = node.col + direction[1];
+
+            if (isValidLocation(row, col) && maze[row][col] == 1) {
+                Node neighbor = nodes[row][col];
+                if (neighbor != null && !neighbors.contains(neighbor)) {
+                    // 대각선 방향인 경우 가중치 1.4를 적용
+                    int neighborGScore = (direction[0] == 0 || direction[1] == 0) ? node.gScore + 1 : node.gScore + 14;
+
+                    if (neighbor.gScore > neighborGScore) {
+                        neighbor.parent = node;
+                        neighbor.gScore = neighborGScore;
+                        neighbor.fScore = neighborGScore + calculateFScore(neighbor, endNode);
+                    }
+
+                    neighbors.add(neighbor);
+                }
+            }
+        }
+
+        return neighbors;
+    }
+
+    private int calculateFScore(Node node, Node endNode) {
+        return node.gScore + calculateHeuristic(node, endNode);
+    }
+
+    private int calculateHeuristic(Node node, Node endNode) {
+        return Math.abs(node.row - endNode.row) + Math.abs(node.col - endNode.col);
+    }
+    public void showPath(Context context, List<Node> path) {
+        if (path == null || path.isEmpty()) {
+            showToast(context, "No path found.");
+            return;
+        }
+
+        StringBuilder pathBuilder = new StringBuilder();
+        for (Node node : path) {
+            pathBuilder.append("(").append(node.row).append(", ").append(node.col).append(")\n");
+        }
+
+        showToast(context, pathBuilder.toString());
+    }
+
+    private void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+
+    public int getNumCols() {
+        return numCols;
+    }
+
+    public int getNumRows() {
+        return numRows;
+    }
+}
+
+
+
+
+
--- app/src/main/java/org/techtown/user/NavigationActivity.java
+++ app/src/main/java/org/techtown/user/NavigationActivity.java
@@ -1,19 +1,44 @@
 package org.techtown.user;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.FragmentTransaction;
+
+import org.w3c.dom.Node;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NavigationActivity extends AppCompatActivity {
-    private TextView selectedItemTextView;
+    int width = 629;
+    int height = 1280; // map 픽셀크기
+    private List<AStarAlgorithm.Node> path = new ArrayList<>(); // 경로 정보
+    private int numRows; // 미로 행 개수
+    private int numCols; // 미로 열 개수
+    private int[][] maze; // 미로 정보를 저장하는 2차원 배열
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,26 +58,112 @@ public class NavigationActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.destination);
         textView.setText(selectedDest);
 
-        // 리셋 버튼 클릭 시 메인 화면으로 이동
-        Button btnReset = findViewById(R.id.resetbtn);
-        btnReset.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(NavigationActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            switch (item.getItemId()){
-                case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
-                    // 액티비티 이동
-                    Intent listIntent = new Intent(NavigationActivity.this, MainActivity.class);
-                    startActivity(listIntent);
-                    finish();
-                    return true;
-                }
-            }
-            return super.onOptionsItemSelected(item);
+        maze = readMazeFromFile();
+        //numRows = maze.length;
+        //numCols = numRows > 0 ? maze[0].length : 0;
+        AStarAlgorithm algorithm = new AStarAlgorithm(maze);
+        path = algorithm.findShortestPath(106, 12, 132, 20); //임의로 지정
+        //algorithm.showPath(this, path); // path toast로 미리 출력
+        if (path != null && !path.isEmpty()) {
+            drawPathOnCanvas(path, numRows, numCols);
+
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: { //toolbar의 back키 눌렀을 때 동작
+                // 액티비티 이동
+                Intent listIntent = new Intent(NavigationActivity.this, MainActivity.class);
+                startActivity(listIntent);
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void drawPathOnCanvas(List<AStarAlgorithm.Node> path, int numRows, int numCols) {
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(20);
+
+        // 배경 이미지 로드
+        Bitmap background = BitmapFactory.decodeResource(getResources(), R.drawable.map4);
+
+        // 캔버스 크기 설정
+        int canvasWidth = background.getWidth();
+        int canvasHeight = background.getHeight();
+        String message = canvasWidth + " x " + canvasHeight;
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+
+        // 빈 비트맵 생성
+        Bitmap bitmap = Bitmap.createBitmap(canvasWidth, canvasHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        // 배경 이미지 그리기
+        canvas.drawBitmap(background, 0, 0, null);
+        // 경로 그리기
+        int startX, startY, endX, endY;
+        AStarAlgorithm.Node prevNode = null;
+        for (int i = 1; i < path.size(); i++) {
+            AStarAlgorithm.Node currentNode = path.get(i);
+            AStarAlgorithm.Node previousNode = path.get(i - 1);
+
+            startX = calculateXCoordinate(previousNode.col, canvasWidth, numCols);
+            startY = calculateYCoordinate(previousNode.row, canvasHeight, numRows);
+            endX = calculateXCoordinate(currentNode.col, canvasWidth, numCols);
+            endY = calculateYCoordinate(currentNode.row, canvasHeight, numRows);
+            canvas.drawLine(startX, startY, endX, endY, paint);
+        }
+
+        ImageView mapViewImageView = findViewById(R.id.mapView);
+        mapViewImageView.setImageBitmap(bitmap);
+    }
+
+    // maze와 map의 크기를 맞춰서 좌표 설정해주는 부분
+    private int calculateXCoordinate(int col, int canvasWidth, int numCols) {
+        return (int) (col * 22.6);
+    }
+    private int calculateYCoordinate(int row, int canvasHeight, int numRows) {
+        return (int) (row * 22.4);
+    }
+
+
+    private int[][] readMazeFromFile() {
+        List<int[]> rows = new ArrayList<>();
+
+        try {
+            InputStream inputStream = getResources().openRawResource(R.raw.maze);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] tokens = line.split(" ");
+                int[] row = new int[tokens.length];
+                for (int i = 0; i < tokens.length; i++) {
+                    row[i] = Integer.parseInt(tokens[i]);
+                }
+                rows.add(row);
+            }
+            reader.close();
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int numRows = rows.size();
+        int numCols = numRows > 0 ? rows.get(0).length : 0;
+        int[][] maze = new int[numRows][numCols];
+        for (int i = 0; i < numRows; i++) {
+            maze[i] = rows.get(i);
+        }
+
+        return maze;
+    }
 }
+
+
+
+
+
