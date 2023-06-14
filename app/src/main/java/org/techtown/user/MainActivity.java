4ce3ac07b7d724e54bcf462ecb3fc6fb46f6ecfd
thxto-me <hjd0907@gmail.com>
2023-06-15 01:35:24 +0900

    fix(): import sort
    Status Update(Jun. 15, 2023 01:30)

--- app/src/main/java/org/techtown/user/MainActivity.java
+++ app/src/main/java/org/techtown/user/MainActivity.java
@@ -1,9 +1,5 @@
 package org.techtown.user;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -21,6 +17,10 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 import java.util.List;
